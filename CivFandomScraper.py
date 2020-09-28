33d7c90062e66e8ef8959c4ffa8aed3611ae177c
conor-mcnally <cmcn@gmx.co.uk>
2020-09-28 21:04:31 +0100

    Return Name, ability, agenda
    So far, 5 seperate lists, need to combine into one with name as key

--- CivFandomScraper.py
+++ CivFandomScraper.py
@@ -2,10 +2,10 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-import urllib.request
 
 #regex expression for html tags
 clean = re.compile('<.*?>')
+
 #Function to strip html tags from text
 def strip_html(list):
 	newList = []
@@ -24,23 +24,16 @@ soup = BeautifulSoup(data.text, 'html.parser')
 list_of_leaders = soup.find('table', { 'class': 'wikitable sortable' })
 #Extract each leader row from table
 leader = list_of_leaders.find_all('tr')
-#Pop out <th> before we begin to eliminate first empty row
-
-#This works - Finds only p tags in list of leaders
-#Retrives Leader ability & leader agenda, need to split by seperate p tags
-# p - finds descriptions for abiliy and agenda
-# b - names of agendas and abilities
-# a - SHOULD find name of leader(td[0]), where td[0] == leader, td[1] == civ, td[2] == ability, td[3] == agenda
+#Remove first row containing table headings
+leader.pop(0)
 
 #-----------------------------------------------------------------------------------
-#LEADER ABILITY/AGENDA TITLE
-#Extract leader ability/agenda titles from table
+#leader ability/agenda titles
 leader_info_titles = []
 for td in leader:
 	leader_info_titles.append(td.find_all('b'))
 
 #Save all agenda/ability titles to lists
-leader_info_titles.pop(0) #First entry is empty, remove
 ability_title = [i[0] for i in leader_info_titles] #Seperate ability to seperate list
 agenda_title = [i[1] for i in leader_info_titles]  #Seperate agenda to seperate list
 
@@ -51,14 +44,12 @@ agenda_title = strip_html(agenda_title)
 #Show results
 print("Ability Title: \n", ability_title[1], "\nAgenda Title: \n", agenda_title[1])
 #------------------------------------------------------------------------------------
-#LEADER ABILITY/AGENDA TEXT
-#Extract leader ability/agenda text from table
+#leader ability/agenda text
 leader_info_text = []
 for td in leader:
 	leader_info_text.append(td.find_all('p'))
 
 #Save all agenda/ability text to lists
-leader_info_text.pop(0) #First entry is empty, remove
 ability_text = [i[0] for i in leader_info_text] #Seperate ability to seperate list
 agenda_text = [i[1] for i in leader_info_text]  #Seperate agenda to seperate list
 
@@ -70,23 +61,34 @@ agenda_text = strip_html(agenda_text)
 print("\nAbility Text: \n", ability_text[1], "\nAgenda Text: \n", agenda_text[1])
 #------------------------------------------------------------------------------------
 #Leader Name
-# leader_name = []
-# for tr in leader:
-# 	name = tr.find_all('td')[0].find_all('a')[1]
-# 	print(name)
+name = []
+for tr in leader:
+	for td in tr:
+		name.append(tr.find_all('a')[1])
+
+#Strip html tags
+name = strip_html(name)
+
+#Remove duplicates
+name = list(dict.fromkeys(name))
+
+print("Leader Name: \n", name[0:10])
 
 #------------------------------------------------------------------------------------
 #Leader Icon
 # leader_icon = []
-
+# for tr in leader:
+# 	for image in tr:
+# 		leader_icon.append(image.find_all('img')['src'])
+# print(len(leader_icon))
 
 #------------------------------------------------------------------------------------
-#Final - Have Seperate lists to be used in embed/ or maybe save to JSON to search by name/key?
-#JSON Structure would be good, index is leader name then
+#Combine Agenda/Ability titles and text
+agenda_dict = dict(zip(agenda_title, agenda_text))
+#print(agenda_dict)
 
-# for tr in list_of_leaders.find_all('tr'):
-# 	leader = tr.find_all('td')[1].text.strip()
-# 	civilization = tr.find_all('td')[2].text.strip()
-# 	leader_ability = tr.find_all('td')[3].text.strip()
-# 	leader_agenda = tr.find_all('td')[4].text.strip()
-# 	print(leader, civilization, leader_ability, leader_agenda)
+ability_dict = dict(zip(ability_title, ability_text))
+#print(ability_dict)
+
+#------------------------------------------------------------------------------------
+#Final - Have Seperate lists to be used in embed
