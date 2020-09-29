84a9121b0158b29ac64ce976e4298a962359698f
conor-mcnally <cmcn@gmx.co.uk>
2020-09-30 00:44:30 +0100

    Added Icons, quick fix for alexander
    Ability_text & agenda_text needs fixed as some work fine but any with an additional paragraph has an index range longer than 1, so additional ability stuff spills over into agenda and actual agenda text dosent show as a result. So i[0] & i[1] for most but for some i == i[0:2] where i[0:1] is ability & i[2] is agenda
    So maybe of len(i)>2, skip i[1]

--- Leaders.py
+++ Leaders.py
@@ -70,17 +70,23 @@ name = strip_html(name)
 
 #Remove duplicates
 name = list(dict.fromkeys(name))
-print("Length of names list:\n", len(name))
+#print("Length of names list:\n", len(name))
+
 #------------------------------------------------------------------------------------
+
 # Leader Icon
 leader_icon = []
 for tr in leader:
 	for td in tr.find_all("a")[0]:
+	#Alexander entry hiding in here
 		for image in td:
-			leader_icon.append(td.get('src'))
-print("Length of icons list:\n", len(leader_icon))
-
+# 		#All other icons here
+		#It skips alexander as it starts with noscript tag, which he does not start with
+			leader_icon.append(image.get('src'))
+#print("Length of icons list:\n", len(leader_icon))
+leader_icon.insert(0, "https://vignette.wikia.nocookie.net/civilization/images/3/33/Alexander_%28Civ6%29.png/revision/latest/scale-to-width-down/44?cb=20180216210702")
 #------------------------------------------------------------------------------------
+
 #Combine Lists
 @dataclass
 class Leader:
@@ -88,20 +94,21 @@ class Leader:
 	ability_text : str
 	agenda_title : str
 	agenda_text : str
+	leader_icon : str
 
 combined = {
-	key : Leader(ability_title, ability_text, agenda_title, agenda_text)
-	for (key, ability_title, ability_text, agenda_title, agenda_text) in zip (name, ability_title, ability_text, agenda_title, agenda_text)
+	key : Leader(ability_title, ability_text, agenda_title, agenda_text, leader_icon)
+	for (key, ability_title, ability_text, agenda_title, agenda_text, leader_icon) in zip (name, ability_title, ability_text, agenda_title, agenda_text, leader_icon)
 }
 
 #Discord Help Attempt - dict comprehension
 #combined = {key : rest for (key, *rest) in zip(name, ability_title, ability_text, agenda_title, agenda_text)}
 
-#Show results
-user_input = input("Enter leader name: ")
-print("\nAbility Title: \n", combined[user_input].ability_title)
-print("\nAbility Text: \n", combined[user_input].ability_text)
-print("\nAgenda Title: \n", combined[user_input].agenda_title)
-print("\nAgenda Text: \n", combined[user_input].agenda_text)
+#Show results - Testing only
+# user_input = input("Enter leader name: ")
+# print("\nAbility Title: \n", combined[user_input].ability_title)
+# print("\nAbility Text: \n", combined[user_input].ability_text)
+# print("\nAgenda Title: \n", combined[user_input].agenda_title)
+# print("\nAgenda Text: \n", combined[user_input].agenda_text)
 
 #------------------------------------------------------------------------------------
